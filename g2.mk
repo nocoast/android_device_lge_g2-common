7b1e3c8700b4458bbb7b72a79245bf8ae87c51ec
houst0nn <houstonn@gmail.com>
2013-10-28 20:09:16 +0000

    some fixes

--- g2.mk
+++ g2.mk
@@ -185,7 +185,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+	wifi.supplicant_scan_interval=120
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
